33ac7424bd01b12cd0d02907a7f041a5dfe12d3f
Michal 'vorner' Vaner <vorner@vorner.cz>
2021-02-05 14:48:59 +0100

    Actually does something

--- src/main/scala/titulky/Titulky.scala
+++ src/main/scala/titulky/Titulky.scala
@@ -4,15 +4,28 @@ import io.Source
 
 object Titulky {
   private val line = raw"(.*) --> (.*)".r
+  private val time = raw"(\d+):(\d+):(\d+),(\d+)".r
 
-  private def decode(t: String): Double = 1.0
-  private def encode(t: Double): String = "Hello"
+  private def decode(t: String): Double = {
+    t match {
+      case time(h, m, s, p) => h.toDouble * 3600.0 + m.toDouble * 60.0 + s.toDouble + (p.toDouble / 1000.0)
+    }
+  }
+  private def encode(t: Double): String = {
+    val hours = (t / 3600.0).toInt
+    val noHours = t - (hours * 3600.0)
+    val minutes = (noHours / 60.0).toInt
+    val seconds = noHours - minutes * 60.0
+    val wholeSeconds = seconds.toInt
+    val parts = ((seconds - wholeSeconds) * 1000.0).toInt
+    "%02d:%02d:%02d,%03d".format(hours, minutes, wholeSeconds, parts)
+  }
 
   private def processLine(l: String, stretch: Double): String = {
     l match {
       case line(start, stop) => {
         val startStretched = encode(decode(start) * stretch)
-        val endStretched = encode(decode(start) * stretch)
+        val endStretched = encode(decode(stop) * stretch)
         s"$startStretched --> $endStretched"
       }
       case _ => l
@@ -20,9 +33,12 @@ object Titulky {
   }
 
   def main(args: Array[String]) {
+    val orig = decode(args(0))
+    val target = decode(args(1))
+    val mod = target / orig
     Source.stdin
       .getLines()
-      .map(processLine(_, 1.0))
+      .map(processLine(_, mod))
       .foreach(println)
   }
 }
